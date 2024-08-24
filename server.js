3aef9cbb83ff3c4274432542c09644b15bf9f262
alexrequeni81 <35029740+alexrequeni81@users.noreply.github.com>
2024-08-24 12:20:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
-const http = require('http');
-const socketIo = require('socket.io');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -36,17 +34,10 @@ const Part = mongoose.model('Part', partSchema, 'databasev1');
 // Configurar la carpeta de archivos estáticos
 app.use(express.static(path.join(__dirname, 'frontend')));
 
-// Emitir cambios a todos los clientes
-const notifyClients = () => {
-    io.emit('dataUpdated');
-};
-
 // Obtener todos los repuestos (API)
 app.get('/api/parts', async (req, res) => {
     try {
-        console.log('Solicitando datos de MongoDB...');
         const parts = await Part.find();
-        console.log('Datos obtenidos:', parts);
         res.json(parts);
     } catch (err) {
         console.error('Error al obtener los repuestos:', err);
@@ -59,7 +50,6 @@ app.post('/api/parts', async (req, res) => {
     try {
         const newPart = new Part(req.body);
         await newPart.save();
-        notifyClients();
         res.status(201).json(newPart);
     } catch (err) {
         console.error('Error al crear un nuevo repuesto:', err);
@@ -71,7 +61,6 @@ app.post('/api/parts', async (req, res) => {
 app.put('/api/parts/:id', async (req, res) => {
     try {
         const updatedPart = await Part.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        notifyClients();
         res.status(200).json(updatedPart);
     } catch (err) {
         console.error('Error al actualizar el repuesto:', err);
@@ -83,7 +72,6 @@ app.put('/api/parts/:id', async (req, res) => {
 app.delete('/api/parts/:id', async (req, res) => {
     try {
         await Part.findByIdAndDelete(req.params.id);
-        notifyClients();
         res.status(204).send();
     } catch (err) {
         console.error('Error al eliminar el repuesto:', err);
@@ -96,17 +84,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
-// Iniciar el servidor con Socket.IO solo una vez
-const server = http.createServer(app);
-const io = socketIo(server);
-
-io.on('connection', (socket) => {
-    console.log('Nuevo cliente conectado');
-    socket.on('disconnect', () => {
-        console.log('Cliente desconectado');
-    });
-});
-
-server.listen(PORT, () => {
+// Iniciar el servidor
+app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
